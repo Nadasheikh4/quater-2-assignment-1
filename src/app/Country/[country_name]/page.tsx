a37a3d9eb76241dab52465b57f8f8ddc7312a9cf
Nada Sheikh <nadakhalilsheikh@gmail.com>
2024-10-13 20:32:35 +0500

    first commit

---
+++ src/app/Country/[country_name]/page.tsx
@@ -0,0 +1,82 @@
+export default function CountryName({params}:any){
+
+
+
+
+const countries:{
+    name:string,
+    population:number,
+    capital:string,
+
+}[]=[{
+    name :"Pakistan",
+    population: 216000000,
+    capital: " Islamabad"},
+{
+    name :"India",
+    population: 1.4170000000,
+    capital: " Dehli"},
+    {
+
+    name :"Palestine",
+    population: 5.04400000,
+    capital: " Jerusalem"},
+
+    {name :"China",
+        population: 1010101010101,
+        capital: " Beijing"},
+                            
+{
+        name :"Turkey",
+    population: 1919191919,
+    capital: " Ankara"},
+
+
+]
+
+function findCountry(Country_url:string){
+    return countries.find(country=>country.name.toLowerCase() == Country_url.toLowerCase());
+
+}
+
+let result = findCountry(params.country_name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <div>
+            {
+            result ? (
+                <>
+             <h1>Country Name: {result.name}</h1>
+             <h1>Country Capital :{result.capital}</h1>
+             <h1>Country Population:{result.population }</h1>
+
+
+
+                </>
+            )
+            :(
+                <h1>Country Not Found</h1>
+            )
+            }
+            
+
+
+
+
+             
+        
+             </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+
+      <h1>Country List</h1>
+
+      
+      <Link href ="/Country/Pakistan"style={{margin:"10px", borderRadius : "10 px", border:"2px solid black",padding :"10px"}}>Pakistan</Link>
+      <Link href = "/Country/India"style={{margin:"10px", borderRadius : "10 px", border:"2px solid black",padding :"10px"}}>India</Link>
+      <Link href ="/Country/Palestine"style={{margin:"10px", borderRadius : "10 px", border:"2px solid black",padding :"10px"}}>Palestine</Link>
+      
+      <Link href ="/Country/China"style={{margin:"10px", borderRadius : "10 px", border:"2px solid black",padding :"10px"}}>China</Link>
+      <Link href= "/Country/Turkey" style={{margin:"10px", borderRadius : "10 px", border:"2px solid black",padding :"10px"}}>Turkey</Link>
+    </div>
+  );
+}
